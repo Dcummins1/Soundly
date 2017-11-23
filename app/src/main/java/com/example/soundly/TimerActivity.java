f5b1cebd74c8cc17880e3f135ea3d27762b14053
connormross <connor.ross@ucdconnect.ie>
2017-11-23 16:09:20 +0000

    Fixed some bugs in the Timer Activity

--- app/src/main/java/com/example/soundly/TimerActivity.java
+++ app/src/main/java/com/example/soundly/TimerActivity.java
@@ -8,12 +8,14 @@ import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class TimerActivity extends AppCompatActivity {
 
     EditText editText;
+    ProgressBar progressBar;
     Button startButton, stopButton;
     TextView textView;
 
@@ -23,17 +25,17 @@ public class TimerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_timer);
 
         editText = (EditText) findViewById(R.id.editText);
+        progressBar=(ProgressBar)findViewById(R.id.progressBar);
         startButton = (Button) findViewById(R.id.startButton);
         stopButton = (Button) findViewById(R.id.stopButton);
         textView = (TextView) findViewById(R.id.textView);
 
-
+        //creates onClickListener for start button.
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                //checks if user has entered a number; displays message when no input has been given.
-                //else, creates new CountDownTimer
+                //checks if there is user input; displays toast when no input has been given.
                 if  (editText.getText().toString().equals("")) {
                     Context context = getApplicationContext();
                     CharSequence textT = "Please enter time";
@@ -42,39 +44,56 @@ public class TimerActivity extends AppCompatActivity {
                     Toast toast = Toast.makeText(context, textT, duration);
                     toast.show();
                 }
+                //if there is user input, checks if the value of the input is 0; displays toast promting the user
+                // for a number > 0 if true.
                 else {
                     String text = editText.getText().toString();
                     int seconds = Integer.valueOf(text);
 
-                    final CountDownTimer countDownTimer = new CountDownTimer(seconds * 60000, 1000) {
-                        @Override
-                        public void onTick(long l) {
-                            textView.setText("Minutes: " + (int) (l / 60000) + " Seconds: " + (int) (l / 1000));
-                        }
+                    if (seconds == 0){
+                        Context context = getApplicationContext();
+                        CharSequence textT = "Please enter valid time input";
+                        int duration = Toast.LENGTH_LONG;
 
-                        @Override
-                        public void onFinish() {
-                            textView.setText("Finished!");
-                        }
-                    };
+                        Toast toast = Toast.makeText(context, textT, duration);
+                        toast.show();
+                    }
+                    //Once program has confirmed that there is a valid input put it creates a new CountDownTimer,
+                    //sets up the progressBar and "freezes" the editText field.
+                    else {
+                        final CountDownTimer countDownTimer = new CountDownTimer(seconds * 60000, 1000) {
+                            @Override
+                            public void onTick(long l) {
+                                textView.setText("Minutes: " + ((int) (l / 60000) + 1));
 
-                    //starts timer
-                    countDownTimer.start();
-                    editText.setText("");
+                                int progress = (int) (l / 1000);
+                                progressBar.setProgress(progress);
+                            }
+                            @Override
+                            public void onFinish() {
+                                textView.setText("Finished!");
+                                editText.setFocusableInTouchMode(true);
+                                System.out.println("finished");
+                            }
+                        };
 
-                    //cancels timer on stopButton click
-                    stopButton.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            countDownTimer.cancel();
-                            textView.setText("");
-                        }
-                    });
+                        //starts timer
+                        countDownTimer.start();
+                        editText.setText("");
+                        editText.setFocusable(false);
+
+                        //cancels timer on stopButton click
+                        stopButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                countDownTimer.cancel();
+                                textView.setText("");
+                                editText.setFocusableInTouchMode(true);
+                            }
+                        });
+                    }
                 }
             }
         });
     }
-
-
-
 }
