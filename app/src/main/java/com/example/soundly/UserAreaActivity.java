6051bac8453669ee5e1d57d3afae04c90b32ab14
connormross <connor.ross@ucdconnect.ie>
2017-11-25 17:09:27 +0000

    Working timer on UserArea page

--- app/src/main/java/com/example/soundly/UserAreaActivity.java
+++ app/src/main/java/com/example/soundly/UserAreaActivity.java
@@ -21,16 +21,15 @@ public class UserAreaActivity extends AppCompatActivity {
     //    ProgressBar progressBar;
     Button timerButton, stopButton, settings, soundly;
     //timerButton was previously startButton
-    TextView textView;
+    TextView textView, tvGraph;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
-        TextView tvGraph = (TextView) findViewById(R.id.tvgraph);
 
-//        Button soundly = (Button) findViewById(R.id.bStart);
-//        Button settings = (Button) findViewById(R.id.bSettings);
+        //@Dan changed tvGraph, soundly and settings slightly
+        tvGraph = (TextView) findViewById(R.id.tvgraph);
         soundly = (Button) findViewById(R.id.bStart);
         settings = (Button) findViewById(R.id.bSettings);
         timerButton = (Button) findViewById(R.id.startButton);
@@ -46,12 +45,6 @@ public class UserAreaActivity extends AppCompatActivity {
             }
         });
 
-//        timer.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(UserAreaActivity.this, TimerActivity.class));
-//            }
-//        });
 
         soundly.setOnClickListener(new View.OnClickListener() {
             @Override
